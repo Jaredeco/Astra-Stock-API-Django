b249850aebc183a06eae6e693e71ef3801ca0917
Jaredeco <jano.redecha@gmail.com>
2023-08-15 15:58:45 +0200

    Create API
    Signed-off-by: Jaredeco <jano.redecha@gmail.com>

--- api/urls.py
+++ api/urls.py
@@ -1,17 +1,8 @@
 from django.urls import path
-from .views import BondListView, BondCRUDView, BondCreateView, AnalyzePortfolioView, AuthenticationView, \
-    PurchaseBondView, UserPortfolioBondListView, UserPortfolioInvestmentListView, InvestmentCRUDView
+from .views import astra_products_number, astra_products_names, astra_product_parts
 
-# decided to use trailing slash
 urlpatterns = [
-    path('authentication/', AuthenticationView.as_view(), name='authentication'),
-    path('bonds/', BondListView.as_view(), name='bond-list'),
-    path('bond/', BondCreateView.as_view(), name='create-bond'),
-    path('bond/<str:isin>/', BondCRUDView.as_view(), name='bond-detail'),
-    path('portfolio/', AnalyzePortfolioView.as_view(), name='analyze-portfolio'),
-    path('portfolio/investments/', UserPortfolioInvestmentListView.as_view(), name='user-portfolio-'
-                                                                                   'investments'),
-    path('portfolio/bonds/', UserPortfolioBondListView.as_view(), name='user-portfolio-bonds'),
-    path('investment/', PurchaseBondView.as_view(), name='purchase-bond'),
-    path('investment/<int:id>/', InvestmentCRUDView.as_view(), name='investment-detail'),
+    path('products_number/', astra_products_number, name='products-number'),
+    path('products/', astra_products_names, name='products-names'),
+    path('product/<product_code>/', astra_product_parts, name='product-parts'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,213 +1,61 @@
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
-from base.models import Bond, Investment
-from .serializers import BondSerializer, UserSerializer, InvestmentSerializer
-from rest_framework import status
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
-from rest_framework.authtoken.models import Token
-from django.db.models import Avg
-from django.utils import timezone
+from rest_framework.status import HTTP_200_OK
 from drf_spectacular.utils import extend_schema
+from urllib.request import urlopen
+from io import BytesIO
+from zipfile import ZipFile
+from xml.etree import ElementTree as et
+from rest_framework.decorators import api_view
 
 
-@extend_schema(description="Retrieve a list of all bonds. (No authentication required)")
-class BondListView(ListAPIView):
-    queryset = Bond.objects.all()
-    serializer_class = BondSerializer
+def download_and_unzip(url, extract_to='.'):
+    http_response = urlopen(url)
+    zipfile = ZipFile(BytesIO(http_response.read()))
+    zipfile.extractall(path=extract_to)
 
 
-@extend_schema(description="Bond detail. (Admin privileges required)")
-class BondCRUDView(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAdminUser,)
-    queryset = Bond.objects.all()
-    serializer_class = BondSerializer
-    lookup_field = 'isin'
+class AstraZip:
+    def __init__(self):
+        file_url = 'https://www.retailys.cz/wp-content/uploads/astra_export_xml.zip'
+        file_path = 'export_full.xml'
+        download_and_unzip(file_url)
+        self.data = et.parse(file_path).getroot()
+        self.products = self.data.find('items').findall('item')
+
+    def get_products_number(self):
+        return len(self.products)
+
+    def get_products_names(self):
+        return [p.attrib['name'] for p in self.products]
+
+    def get_products_parts(self, product_code):
+        for p in self.products:
+            if p.attrib['code'] == product_code:
+                parts = p.find('parts')
+                if parts is not None:
+                    return [pt.attrib['name'] for pt in parts.findall('part')]
+        return []
 
 
-@extend_schema(description="Create a new bond. (Admin privileges required)")
-class BondCreateView(CreateAPIView):
-    permission_classes = (IsAdminUser,)
-    queryset = Bond.objects.all()
-    serializer_class = BondSerializer
+astra_zip = AstraZip()
 
 
-@extend_schema(description="Retrieve a list of all users. (Admin privileges required)")
-class UsersListView(ListAPIView):
-    permission_classes = (IsAdminUser,)
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+@extend_schema(description="Get number of products in Astra")
+@api_view(('GET',))
+def astra_products_number(request):
+    products_number = astra_zip.get_products_number()
+    return Response(data=products_number, status=HTTP_200_OK)
 
 
-@extend_schema(description="Analyze the investment portfolio of the authenticated user. (User authentication required)")
-class AnalyzePortfolioView(APIView):
-    permission_classes = (IsAuthenticated,)
-
-    def get(self, request):
-        investments = Investment.objects.filter(username=request.user.username)
-        if len(investments) == 0:
-            return Response(status=status.HTTP_204_NO_CONTENT)
-
-        today = timezone.now()
-        average_interest_rate = investments.aggregate(avg=Avg('bond_isin__interest_rate'))['avg']
-        not_expired_bonds = investments.filter(bond_isin__expiration_date__gte=today)
-        soon_expires = not_expired_bonds.order_by('bond_isin__expiration_date').first()
-        portfolio_balance = 0
-        future_portfolio_balance = 0
-
-        # different possible solution, but current seems more intuitive and clear for me
-        # from django.db.models import F, Sum, Case, When
-        # portfolio_balance = investments.aggregate(
-        #     balance=Sum(
-        #         F('volume') * F('bond_isin__value')
-        #     )
-        # )['balance'] or 0
-        #
-        # future_portfolio_balance = investments.aggregate(
-        #     future_balance=Sum(
-        #         Case(
-        #             When(bond_isin__expiration_date__lt=today, then=F('volume') * F('bond_isin__value')),
-        #             default= F('volume') * (F('bond_isin__value') +
-        #                                    (F('bond_isin__value') * F('bond_isin__interest_rate')))
-        #             ,
-        #         )
-        #     )
-        # )['future_balance'] or 0
-
-        for investment in investments:
-            portfolio_balance += investment.volume * investment.bond_isin.value
-            if investment.bond_isin.expiration_date >= today:
-                current_value = investment.volume * investment.bond_isin.value
-                expected_return_one_year = investment.volume * investment.bond_isin.value * investment.bond_isin.interest_rate
-                future_portfolio_balance += current_value + expected_return_one_year
-            else:
-                future_portfolio_balance += investment.volume * investment.bond_isin.value
-
-        data = {
-            'average_interest_rate': average_interest_rate,
-            'soon_expires': 'All bods in your portfolio have already expired!',
-            'portfolio_balance': portfolio_balance,
-            'future_portfolio_balance': future_portfolio_balance,
-        }
-
-        if soon_expires is not None:
-            data['soon_expires'] = {
-                'bond_name': soon_expires.bond_isin.name,
-                'bond_isin': soon_expires.bond_isin.isin,
-                'bond_expiration_date': soon_expires.bond_isin.expiration_date,
-            }
-
-        return Response(data, status=status.HTTP_200_OK)
+@extend_schema(description="Get all products names in Astra")
+@api_view(('GET',))
+def astra_products_names(request):
+    products_names = astra_zip.get_products_names()
+    return Response(data=products_names, status=HTTP_200_OK)
 
 
-@extend_schema(description="Purchase a bond by providing the username, bond ISIN, and volume. (User authentication "
-                           "required)")
-class PurchaseBondView(APIView):
-    permission_classes = (IsAuthenticated,)
-    serializer_class = InvestmentSerializer
-
-    def post(self, request):
-        data = request.data
-        username = data.get('username')
-        bond_isin = data.get('bond_isin')
-        volume = int(data.get('volume'))
-
-        try:
-            User.objects.get(username=username)
-        except User.DoesNotExist:
-            return Response({'error': 'User not found'}, status=404)
-
-        try:
-            Bond.objects.get(isin=bond_isin)
-        except Bond.DoesNotExist:
-            return Response({'error': 'Bond not found'}, status=404)
-
-        if volume <= 0:
-            return Response({'error': 'Invalid volume'}, status=400)
-
-        investment_data = {
-            'username': username,
-            'bond_isin': bond_isin,
-            'volume': volume,
-        }
-
-        serializer = InvestmentSerializer(data=investment_data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
-
-
-@extend_schema(description="Investment details (User Authentication required)")
-class InvestmentCRUDView(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
-    serializer_class = InvestmentSerializer
-    lookup_field = 'id'
-
-    def get_queryset(self):
-        return Investment.objects.filter(username=self.request.user.username)
-
-
-@extend_schema(description="Retrieve a list of all bonds in portfolio of the authenticated user. (User authentication "
-                           "required)")
-class UserPortfolioBondListView(ListAPIView):
-    permission_classes = (IsAuthenticated,)
-    serializer_class = BondSerializer
-
-    def get_queryset(self):
-        investment_query_set = Investment.objects.filter(username=self.request.user.username)
-        return Bond.objects.filter(isin__in=investment_query_set.values('bond_isin'))
-
-
-@extend_schema(description="Retrieve a list of all investments in portfolio of the authenticated user. (User "
-                           "authentication required)")
-class UserPortfolioInvestmentListView(ListAPIView):
-    permission_classes = (IsAuthenticated,)
-    queryset = Bond.objects.all()
-    serializer_class = InvestmentSerializer
-
-    def get_queryset(self):
-        return Investment.objects.filter(username=self.request.user.username)
-
-
-@extend_schema(description="Register the user if no account exists or authenticate if the user already exists.")
-class AuthenticationView(APIView):
-    serializer_class = UserSerializer
-
-    def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        if not username or not password:
-            return Response(
-                {
-                    'username, password': [
-                        'This fields are required.'
-                    ]
-                },
-                status.HTTP_400_BAD_REQUEST
-            )
-
-        user = authenticate(username=username, password=password)
-        if not user:
-            try:
-                User.objects.create_user(username=username, password=password, is_staff=True)
-                user = authenticate(username=username, password=password)
-            except:
-                return Response(
-                    {
-                        'detail': [
-                            'Invalid Credentials'
-                        ]
-                    },
-                    status=status.HTTP_400_BAD_REQUEST
-                )
-
-        token, _ = Token.objects.get_or_create(user=user)
-        return Response(
-            {
-                'token': token.key
-            },
-            status=status.HTTP_200_OK
-        )
+@extend_schema(description="Get parts of a specific product")
+@api_view(('GET',))
+def astra_product_parts(request, product_code):
+    product_parts = astra_zip.get_products_parts(product_code)
+    return Response(data=product_parts, status=HTTP_200_OK)
